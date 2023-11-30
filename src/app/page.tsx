314064d8fd0e1298a63ce9a92e76d077f76b5b98
Neeraj Dalal <admin@nrjdalal.com>
2023-11-30 14:58:27 +0530

    more websites

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ const Page = () => {
       </div>
 
       {
-        // ~ Skills
+        // ~ rdt.li
       }
       <div className="mx-5 flex flex-col border-t border-slate-300 lg:py-10">
         <div className="grid gap-x-16 gap-y-12 py-20 lg:grid-cols-2">
@@ -39,7 +39,12 @@ const Page = () => {
               <div className="h-3.5 w-3.5 rounded-full bg-yellow-400" />
               <div className="h-3.5 w-3.5 rounded-full bg-green-400" />
             </div>
-            <Image src="/rdt.li.png" alt="Next.js" width={1080} height={1920} />
+            <Image
+              src="/rdt.li.png"
+              alt="Redirect Link"
+              width={1080}
+              height={1920}
+            />
           </div>
 
           <div className="flex flex-col justify-center font-medium">
@@ -52,6 +57,82 @@ const Page = () => {
 
             <Link
               href={'https://rdt.li'}
+              target="_blank"
+              className="mt-8 w-max rounded-xl border-2 border-amber-600 px-4 py-2 text-amber-600"
+            >
+              Visit Website
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      {
+        // ~ serpwe.com
+      }
+      <div className="mx-5 flex flex-col border-t border-slate-300 lg:py-10">
+        <div className="grid gap-x-16 gap-y-12 py-20 lg:grid-cols-2">
+          <div className="overflow-hidden rounded-lg border border-slate-300">
+            <div className="bg-slate flex h-7 w-full items-center gap-x-2 px-3">
+              <div className="h-3.5 w-3.5 rounded-full bg-red-400" />
+              <div className="h-3.5 w-3.5 rounded-full bg-yellow-400" />
+              <div className="h-3.5 w-3.5 rounded-full bg-green-400" />
+            </div>
+            <Image
+              src="/serpwe.com.png"
+              alt="Serpwe"
+              width={1080}
+              height={1920}
+            />
+          </div>
+
+          <div className="flex flex-col justify-center font-medium">
+            <h2 className="text-2xl md:text-3xl">Serpwe</h2>
+
+            <p className="pt-8 text-xl text-slate-500">
+              Generate keyword ideas, group similar keywords, and organize them
+              into topical clusters - a product of Warewe
+            </p>
+
+            <Link
+              href={'https://serpwe.com'}
+              target="_blank"
+              className="mt-8 w-max rounded-xl border-2 border-amber-600 px-4 py-2 text-amber-600"
+            >
+              Visit Website
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      {
+        // ~ hetrolink.com
+      }
+      <div className="mx-5 flex flex-col border-t border-slate-300 lg:py-10">
+        <div className="grid gap-x-16 gap-y-12 py-20 lg:grid-cols-2">
+          <div className="overflow-hidden rounded-lg border border-slate-300">
+            <div className="bg-slate flex h-7 w-full items-center gap-x-2 px-3">
+              <div className="h-3.5 w-3.5 rounded-full bg-red-400" />
+              <div className="h-3.5 w-3.5 rounded-full bg-yellow-400" />
+              <div className="h-3.5 w-3.5 rounded-full bg-green-400" />
+            </div>
+            <Image
+              src="/hetrolinks.com.png"
+              alt="Serpwe"
+              width={1080}
+              height={1920}
+            />
+          </div>
+
+          <div className="flex flex-col justify-center font-medium">
+            <h2 className="text-2xl md:text-3xl">Hetrolinks</h2>
+
+            <p className="pt-8 text-xl text-slate-500">
+              Instantly Repair Broken Amazon Affiliate Links - a product of
+              Warewe
+            </p>
+
+            <Link
+              href={'https://hetrolinks.com'}
               target="_blank"
               className="mt-8 w-max rounded-xl border-2 border-amber-600 px-4 py-2 text-amber-600"
             >
