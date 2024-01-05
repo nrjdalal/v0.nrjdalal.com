ebb7877167c02083a94b41b59b698c2b2267dc75
Neeraj Dalal <admin@nrjdalal.com>
2024-01-06 01:22:52 +0530

    more updates

--- src/app/(mdx)/layout.tsx
+++ src/app/(mdx)/layout.tsx
@@ -1,11 +1,17 @@
 'use client'
 
-import { Github, Twitter } from 'lucide-react'
+import { Github, Pencil, Twitter } from 'lucide-react'
 import Link from 'next/link'
 import { useSelectedLayoutSegments } from 'next/navigation'
 
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   const segment = useSelectedLayoutSegments()
+  const githubLink = `https://github.com/nrjdalal/nrjdalal.com/tree/main/src/app/(mdx)/${segment.join(
+    '/',
+  )}/page.mdx`
+  const fileInfoLink = `https://api.github.com/repos/nrjdalal/nrjdalal.com/commits?path=src/app/(mdx)/${segment.join(
+    '/',
+  )}/page.mdx&page=1&per_page=1`
 
   return (
     <main className="container mx-auto min-h-[100dvh] max-w-screen-xl">
@@ -32,16 +38,26 @@ export default function MdxLayout({ children }: { children: React.ReactNode }) {
 
       <div className="flex justify-center">
         <article className="prose w-full max-w-screen-md px-5 pb-20 pt-10 prose-a:font-normal prose-a:no-underline">
-          <div className="flex items-center gap-5">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-5">
+              <Link
+                className="text-xs font-semibold text-blue-500 no-underline"
+                href={'/'}
+              >
+                BACK
+              </Link>
+              <p className="border-px select-none rounded-md bg-slate-100 px-3 py-1 text-xs text-slate-500">
+                {segment.join(' / ')}
+              </p>
+            </div>
+
             <Link
-              className="text-xs font-semibold text-blue-500 no-underline"
-              href={'/'}
+              className="flex items-center gap-2 text-xs"
+              href={githubLink}
+              target="_blank"
             >
-              BACK
+              Edit <Pencil className="h-3.5 w-3.5 text-slate-500" />
             </Link>
-            <p className="border-px select-none rounded-md bg-slate-100 px-3 py-1 text-xs text-slate-500">
-              {segment.join(' / ')}
-            </p>
           </div>
 
           {children}
