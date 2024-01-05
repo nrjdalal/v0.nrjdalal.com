baeb403b5d7e97b52692c84a73dee0f68d1442db
Neeraj Dalal <admin@nrjdalal.com>
2024-01-05 10:23:04 +0530

    blog updated

--- src/components/blogElements.tsx
+++ src/components/blogElements.tsx
@@ -23,9 +23,9 @@ export const BrowserPreview = ({
   return (
     <div>
       <div className="flex h-8 cursor-not-allowed items-center gap-x-2 rounded-t-lg bg-zinc-700 px-3">
-        <div className="h-3 w-3 rounded-full bg-red-400" />
-        <div className="h-3 w-3 rounded-full bg-yellow-400" />
-        <div className="h-3 w-3 rounded-full bg-green-400" />
+        <div className="mt-0.5 h-3 w-3 rounded-full bg-red-400" />
+        <div className="mt-0.5 h-3 w-3 rounded-full bg-yellow-400" />
+        <div className="mt-0.5 h-3 w-3 rounded-full bg-green-400" />
         <div className="ml-3 mt-1 flex h-7 items-center gap-2 rounded-t-md bg-zinc-600 px-3 text-xs text-white">
           <Globe2 className="h-3.5 w-3.5" />
           {title}
