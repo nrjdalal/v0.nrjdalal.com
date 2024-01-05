4e0242ec8259890e924aca043449b5e1ee4972d0
Neeraj Dalal <admin@nrjdalal.com>
2024-01-05 06:14:55 +0530

    added browserPreview

--- src/components/browserPreview.tsx
+++ src/components/browserPreview.tsx
@@ -12,9 +12,9 @@ export const BrowserPreview = ({
   return (
     <div className="w-full rounded-lg border border-slate-300">
       <div className="flex h-8 cursor-not-allowed items-center gap-x-2 rounded-t-lg bg-zinc-700 px-3">
-        <div className="h-3.5 w-3.5 rounded-full bg-red-400" />
-        <div className="h-3.5 w-3.5 rounded-full bg-yellow-400" />
-        <div className="h-3.5 w-3.5 rounded-full bg-green-400" />
+        <div className="h-3 w-3 rounded-full bg-red-400" />
+        <div className="h-3 w-3 rounded-full bg-yellow-400" />
+        <div className="h-3 w-3 rounded-full bg-green-400" />
         <div className="ml-3 mt-1 flex h-7 items-center rounded-t-md bg-zinc-600 px-3 text-xs text-white">
           {title} <X className="ml-1.5 h-3.5 w-3.5" />
         </div>
