f3d6900b047482f63194a2dcf4211611b98b4b48
Neeraj Dalal <admin@nrjdalal.com>
2024-01-10 09:54:01 +0530

    adding analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import { fontMono, fontSans } from '@/lib/fonts'
 import { cn } from '@/lib/utils'
 import type { Metadata } from 'next'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: {
@@ -27,6 +28,16 @@ export default function RootLayout({
       >
         {children}
       </body>
+
+      {/* optional: umami analytics */}
+      {process.env.NEXT_PUBLIC_UMAMI_URL && (
+        <Script
+          async
+          defer
+          src={`${process.env.NEXT_PUBLIC_UMAMI_URL}/script.js`}
+          data-website-id={`${process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}`}
+        />
+      )}
     </html>
   )
 }
